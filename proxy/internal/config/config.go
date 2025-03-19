e76c3a91b424c4223db17848b1029fe964307d0f
amirhnajafiz <najafizadeh21@gmail.com>
2025-03-19 17:31:35 -0400

    add: config package

---
+++ proxy/internal/config/config.go
@@ -0,0 +1,20 @@
+package config
+
+import "github.com/caarlos0/env/v10"
+
+// Config represents the configuration for the application
+type Config struct {
+	Port  int  `env:"TELESCOPE_PORT" envDefault:"8080"`
+	Debug bool `env:"TELESCOPE_DEBUG" envDefault:"false"`
+}
+
+// LoadConfigs loads the configuration from environment variables
+func LoadConfigs() (*Config, error) {
+	// load the configuration from environment variables
+	cfg := Default()
+	if err := env.Parse(cfg); err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
---
+++ proxy/internal/config/default.go
@@ -0,0 +1,9 @@
+package config
+
+// Default returns the default configuration for the application
+func Default() *Config {
+	return &Config{
+		Port:  3000,
+		Debug: true,
+	}
+}
