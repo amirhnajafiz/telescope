6493e50e72486ad50eaec01eb5d916a3340eda11
amirhnajafiz <najafizadeh21@gmail.com>
2025-03-19 17:19:59 -0400

    add: CORS miidleware for fiber app

--- proxy/internal/api/api.go
+++ proxy/internal/api/api.go
@@ -1,10 +1,19 @@
 package api
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
 
 // API struct holds endpoint functions of the proxy server
 type API struct{}
 
 // Register method takes a fiber.App instance and defines all the endpoints
 func (a *API) Register(app *fiber.App) {
+	// enable CORS for all routes
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowMethods: "GET, POST, PUT, DELETE, OPTIONS",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 }
