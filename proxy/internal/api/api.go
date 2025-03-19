2bf641678661f100514b0979fe2fefe1f8a7f461
amirhnajafiz <najafizadeh21@gmail.com>
2025-03-19 17:24:16 -0400

    add: logs middleware for fiber endpoints

--- proxy/internal/api/api.go
+++ proxy/internal/api/api.go
@@ -3,6 +3,7 @@ package api
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 // API struct holds endpoint functions of the proxy server
@@ -10,10 +11,16 @@ type API struct{}
 
 // Register method takes a fiber.App instance and defines all the endpoints
 func (a *API) Register(app *fiber.App) {
-	// enable CORS for all routes
+	// enable CORS for all endpoints
 	app.Use(cors.New(cors.Config{
 		AllowOrigins: "*",
 		AllowMethods: "GET, POST, PUT, DELETE, OPTIONS",
 		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
+	// enable logging for all endpoints
+	app.Use(logger.New(logger.Config{
+		TimeZone: "Local",
+		Format:   "${time} - ${status} - ${method} ${path} ${latency}\n",
 	}))
 }
