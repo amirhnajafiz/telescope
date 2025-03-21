03e7e09b5fd74fa3a44692b4be356082f63027a9
amirhnajafiz <najafizadeh21@gmail.com>
2025-03-21 12:40:40 -0400

    init: content.go

---
+++ proxy/internal/api/content.go
@@ -0,0 +1,23 @@
+package api
+
+import "github.com/gofiber/fiber/v2"
+
+// uploadContent handles the upload of content, used for uploading
+func (a *API) uploadContent(ctx *fiber.Ctx) error {
+	return nil
+}
+
+// downloadContent handles the download of content, used for getting mpd files
+func (a *API) downloadContent(ctx *fiber.Ctx) error {
+	return nil
+}
+
+// listContents handles the listing of all contents
+func (a *API) listContents(ctx *fiber.Ctx) error {
+	return nil
+}
+
+// streamContent handles the streaming of content over DASH
+func (a *API) streamContent(ctx *fiber.Ctx) error {
+	return nil
+}
