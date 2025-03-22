15d821ad14bf2d58727d147a69837e2b17441d1e
amirhnajafiz <najafizadeh21@gmail.com>
2025-03-22 19:31:41 -0400

    add: cmd.go

---
+++ proxy/cmd/api.go
@@ -0,0 +1,46 @@
+package cmd
+
+import (
+	"github.com/amirhnajafiz/telescope/internal/api"
+	"github.com/amirhnajafiz/telescope/internal/config"
+	"github.com/amirhnajafiz/telescope/internal/logr"
+	"github.com/amirhnajafiz/telescope/internal/telemetry/metrics"
+	"github.com/amirhnajafiz/telescope/internal/telemetry/tracing"
+
+	"go.opentelemetry.io/otel/trace"
+)
+
+// RegisterAPI creates a new API instance
+func RegisterAPI(cfg *config.Config) (*api.API, error) {
+	// create a new logger instance
+	logger, err := logr.NewZapLogger(cfg.Debug)
+	if err != nil {
+		return nil, err
+	}
+
+	// create a new otel tracer
+	var tr trace.Tracer
+	if len(cfg.Jaeger) > 0 {
+		tr, err = tracing.NewProductionTracer(cfg.Jaeger)
+	} else {
+		tr, err = tracing.NewDevelopmentTracer()
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	// create new metrics struct
+	metricsInstance := metrics.NewMetrics()
+
+	// check if metrics port is set
+	if cfg.MetricsPort != 0 {
+		metrics.NewServer(cfg.MetricsPort)
+	}
+
+	// create a new API instance
+	return &api.API{
+		Logr:    logger.Named("api"),
+		Metrics: metricsInstance,
+		Tracer:  tr,
+	}, nil
+}
--- proxy/main.go
+++ proxy/main.go
@@ -3,14 +3,10 @@ package main
 import (
 	"fmt"
 
-	"github.com/amirhnajafiz/telescope/internal/api"
+	"github.com/amirhnajafiz/telescope/cmd"
 	"github.com/amirhnajafiz/telescope/internal/config"
-	"github.com/amirhnajafiz/telescope/internal/logr"
-	"github.com/amirhnajafiz/telescope/internal/telemetry/metrics"
-	"github.com/amirhnajafiz/telescope/internal/telemetry/tracing"
 
 	"github.com/gofiber/fiber/v2"
-	"go.opentelemetry.io/otel/trace"
 )
 
 func main() {
@@ -20,45 +16,19 @@ func main() {
 		panic(err)
 	}
 
-	// create a new logger instance
-	logger, err := logr.NewZapLogger(cfg.Debug)
-	if err != nil {
-		panic(err)
-	}
-
-	// create a new otel tracer
-	var tr trace.Tracer
-	if len(cfg.Jaeger) > 0 {
-		tr, err = tracing.NewProductionTracer(cfg.Jaeger)
-	} else {
-		tr, err = tracing.NewDevelopmentTracer()
-	}
-	if err != nil {
-		panic(err)
-	}
-
-	// create new metrics struct
-	metricsInstance := metrics.NewMetrics()
-
-	// check if metrics port is set
-	if cfg.MetricsPort != 0 {
-		metrics.NewServer(cfg.MetricsPort)
-	}
-
 	// create a new fiber app
 	app := fiber.New()
 
 	// create a new API instance
-	apiInstance := api.API{
-		Logr:    logger.Named("api"),
-		Metrics: metricsInstance,
-		Tracer:  tr,
+	apiInstance, err := cmd.RegisterAPI(cfg)
+	if err != nil {
+		panic(err)
 	}
 
 	// register the API endpoints
 	apiInstance.Register(app)
 
-	// start the server on port 3000
+	// start the server on a specified port
 	if err := app.Listen(fmt.Sprintf(":%d", cfg.Port)); err != nil {
 		panic(err)
 	}
