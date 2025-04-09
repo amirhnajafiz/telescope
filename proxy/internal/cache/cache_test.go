e6fd6453df266756c4db1d593b4609212cb87924
IliyaMi <iliya.mi78@gmail.com>
2025-04-09 02:19:57 -0400

    Improve cache test

--- proxy/internal/cache/cache_test.go
+++ proxy/internal/cache/cache_test.go
@@ -14,4 +14,8 @@ func TestSegmentCache(t *testing.T) {
 	if !c.IsCached("abc") {
 		t.Error("expected abc to be cached")
 	}
+
+	if size := c.Size(); size != 1 {
+		t.Errorf("expected size to be 1, got %d", size)
+	}
 }
