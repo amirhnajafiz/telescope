aef1dec13b2a2f5afcbc84a81b294b48e79bb1fe
amirhnajafiz <najafizadeh21@gmail.com>
2025-04-16 14:36:05 -0400

    add: file methods for cache

---
+++ proxy/internal/storage/cache/file.go
@@ -0,0 +1,60 @@
+package cache
+
+import "os"
+
+// create a new file with the given path and content
+func writeToFile(path string, content []byte) error {
+	// open the file for writing
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// write the content to the file
+	_, err = file.Write(content)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// read the content of a file
+func readFromFile(path string) ([]byte, error) {
+	// open the file for reading
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// read the content from the file
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return content, nil
+}
+
+// check if a file exists
+func isFileExists(path string) bool {
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return false
+	}
+
+	return true
+}
+
+// delete a file
+func deleteFile(path string) error {
+	// remove the file
+	err := os.Remove(path)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
