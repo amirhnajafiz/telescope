e4d533dd6ff88071d309e2d9f8fa8323ec3a1d0a
amirhnajafiz <najafizadeh21@gmail.com>
2025-04-17 13:56:09 -0400

    update: cache module

--- proxy/internal/storage/cache/cache.go
+++ proxy/internal/storage/cache/cache.go
@@ -1,6 +1,10 @@
 package cache
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/amirhnajafiz/telescope/pkg/files"
+)
 
 // Cache is an interface for our cache system to store segment records by CID
 type Cache struct {
@@ -16,20 +20,20 @@ func NewCache(bd string) *Cache {
 
 // Store stores the data in the cache under the given CID
 func (c *Cache) Store(cid string, data []byte) error {
-	return writeToFile(fmt.Sprintf("%s/%s", c.baseDir, cid), data)
+	return files.Write(fmt.Sprintf("%s/%s", c.baseDir, cid), data)
 }
 
 // Retrieve retrieves the data from the cache, if it does not exist, it returns an error
 func (c *Cache) Retrieve(cid string) ([]byte, error) {
 	path := fmt.Sprintf("%s/%s", c.baseDir, cid)
-	if !isFileExists(path) {
+	if !files.Exists(path) {
 		return nil, fmt.Errorf("file %s does not exist", cid)
 	}
 
-	return readFromFile(path)
+	return files.Read(path)
 }
 
 // Size returns the number of files in the cache directory
 func (c *Cache) Size() (int, error) {
-	return countFilesInDir(c.baseDir)
+	return files.CountInDir(c.baseDir)
 }
