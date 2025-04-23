950dd59eab4fe2b4fd2542a1ad1d1ea377c37e19
amirhnajafiz <najafizadeh21@gmail.com>
2025-04-23 11:07:26 -0400

    update: bootstrap IPFS methods

--- bootstrap/internal/ipfs/client.go
+++ bootstrap/internal/ipfs/client.go
@@ -2,8 +2,7 @@ package ipfs
 
 // Client is an interface for IPFS client operations
 type Client interface {
-	Get(cid string) ([]byte, error)
-	Put(data []byte) (string, error)
+	PutDIR(path string) (string, error)
 }
 
 // NewClient creates a new IPFS gateway instance
--- bootstrap/internal/ipfs/gateway.go
+++ bootstrap/internal/ipfs/gateway.go
@@ -1,8 +1,7 @@
 package ipfs
 
 import (
-	"bytes"
-	"io"
+	"fmt"
 
 	shell "github.com/ipfs/go-ipfs-api"
 )
@@ -12,36 +11,15 @@ type gateway struct {
 	url string
 }
 
-// Get retrieves data from IPFS using the provided CID
-func (g *gateway) Get(cid string) ([]byte, error) {
+// PutDIR uploads a directory to IPFS and returns the CID
+func (g *gateway) PutDIR(path string) (string, error) {
 	// connect to the IPFS node
 	sh := shell.NewShell(g.url)
 
-	// get the data from IPFS
-	reader, err := sh.Cat(cid)
+	// upload the directory
+	cid, err := sh.AddDir(path)
 	if err != nil {
-		return nil, err
-	}
-	defer reader.Close()
-
-	// read the data from the reader
-	data, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, err
-	}
-
-	return data, nil
-}
-
-// Put uploads data to IPFS and returns the CID
-func (g *gateway) Put(data []byte) (string, error) {
-	// connect to the IPFS node
-	sh := shell.NewShell(g.url)
-
-	// add the data to IPFS
-	cid, err := sh.Add(bytes.NewReader(data))
-	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to upload directory: %w", err)
 	}
 
 	return cid, nil
